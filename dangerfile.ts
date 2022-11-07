d79a1ef4aebc30a0fdd0ab9643d0c3e22e0d2a58
Markim Shaw <ms79723@gmail.com>
2022-11-07 12:15:15 -0600

    fix assignee check

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,10 +4,12 @@ const docs = danger.git.fileMatch('**/*.md')
 const app = danger.git.fileMatch('src/**/*.ts')
 const tests = danger.git.fileMatch('*/__tests__/*')
 
+const assignee = danger.github.pr.assignee
+
 const modifiedMD = danger.git.modified_files.join('- ')
 message('Changed Files in this PR: \n - ' + modifiedMD)
 
 /* Check that an assignee exists */
-if (danger.github.pr.assignee === undefined) {
+if (assignee === null) {
   fail('This PR needs an assignee to pass')
 }
