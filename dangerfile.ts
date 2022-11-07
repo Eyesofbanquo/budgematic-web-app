e3cb26015620f3c3d79991edc8e716a427d9d08b
Markim Shaw <ms79723@gmail.com>
2022-11-07 12:17:50 -0600

    Add label logic

--- dangerfile.ts
+++ dangerfile.ts
@@ -5,6 +5,7 @@ const app = danger.git.fileMatch('src/**/*.ts')
 const tests = danger.git.fileMatch('*/__tests__/*')
 
 const assignee = danger.github.pr.assignee
+const labelExists = danger.github.issue.labels.length > 0
 
 const modifiedMD = danger.git.modified_files.join('- ')
 message('Changed Files in this PR: \n - ' + modifiedMD)
@@ -12,4 +13,8 @@ message('Changed Files in this PR: \n - ' + modifiedMD)
 /* Check that an assignee exists */
 if (assignee === null) {
   fail('This PR needs an assignee to pass')
+}
+
+if (!labelExists) {
+  fail('This PR needs a label')
 }
