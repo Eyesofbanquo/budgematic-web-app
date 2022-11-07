96491f5d5e87b822cdce6540b6279a41e7697339
Markim Shaw <ms79723@gmail.com>
2022-11-07 12:11:18 -0600

    add assignee failure

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,13 @@
-import { message, danger } from 'danger'
+import { message, danger, fail } from 'danger'
+
+const docs = danger.git.fileMatch('**/*.md')
+const app = danger.git.fileMatch('src/**/*.ts')
+const tests = danger.git.fileMatch('*/__tests__/*')
 
 const modifiedMD = danger.git.modified_files.join('- ')
 message('Changed Files in this PR: \n - ' + modifiedMD)
+
+/* Check that an assignee exists */
+if (danger.github.pr.assignee === undefined) {
+  fail('This PR needs an assignee to pass')
+}
