410f160f449bf4910af565673cff115e9c3999e0
Markim Shaw <ms79723@gmail.com>
2022-11-10 19:23:11 -0600

    add database url

--- config/config.js
+++ config/config.js
@@ -17,17 +17,8 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
   },
 }
